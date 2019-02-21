21d9cba55ef85628955178ddcdc73e9dee5c35a9
Dave Cridland <dave@cridland.net>
2019-02-21 15:17:32 +0000

    Don't resume at termination point in awaiters from signal
    Otherwise you get a destroy-when-locked on Windows, which
    sensibly terminates.

--- sigslot/tasklet.h
+++ sigslot/tasklet.h
@@ -10,7 +10,10 @@
 #include <string>
 
 namespace sigslot {
+    template<typename T> struct tasklet;
+
     namespace internal {
+        template<typename T> struct awaitable;
 
         template<typename handle_type>
         struct tasklet {
@@ -80,6 +83,49 @@ namespace sigslot {
             }
         };
 
+        template<typename T>
+        struct awaitable {
+            std::experimental::coroutine_handle<> awaiting = nullptr;
+            sigslot::tasklet<T> const &task;
+
+            awaitable(sigslot::tasklet<T> const &t) : task(t) {
+                task.coro.promise().await_add(this);
+            }
+
+            awaitable(awaitable const &a) : task(a.task) {
+                task.coro.promise().await_add(this);
+            }
+
+            awaitable(awaitable &&other) noexcept : task(other.task) {
+                task.coro.promise().await_add(this);
+            }
+
+            bool await_ready() {
+                if (!task.started()) {
+                    // Need to start the task
+                    const_cast<sigslot::tasklet<T> &>(task).start();
+                }
+                return !task.running();
+            }
+
+            void await_suspend(std::experimental::coroutine_handle<> h) {
+                // The awaiting coroutine is already suspended.
+                awaiting = h;
+            }
+
+            auto await_resume() {
+                return task.get();
+            }
+
+            void resolve() {
+                if (awaiting) awaiting.resume();
+            }
+
+            ~awaitable() {
+                task.coro.promise().await_del(this);
+            }
+        };
+
 
         struct promise_type_base {
             std::string name;
@@ -121,9 +167,18 @@ namespace sigslot {
         template<typename R, typename T>
         struct promise_type : public promise_type_base {
             T value;
+            std::set<awaitable<T> *> awaiters;
             typedef std::experimental::coroutine_handle<promise_type<R, T>> handle_type;
 
             promise_type() : value() {}
+
+            void await_add(awaitable<T> * a) {
+                awaiters.insert(a);
+            }
+
+            void await_del(awaitable<T> * a) {
+                awaiters.erase(a);
+            }
 
             auto get_return_object() {
                 return R{handle_type::from_promise(*this)};
@@ -143,6 +198,15 @@ namespace sigslot {
         template<typename R>
         struct promise_type<R, void> : public promise_type_base {
             typedef std::experimental::coroutine_handle<promise_type<R, void>> handle_type;
+            std::set<awaitable<void> *> awaiters;
+
+            void await_add(awaitable<void> * a) {
+                awaiters.insert(a);
+            }
+
+            void await_del(awaitable<void> * a) {
+                awaiters.erase(a);
+            }
 
             auto get_return_object() {
                 return R{handle_type::from_promise(*this)};
@@ -163,44 +227,7 @@ namespace sigslot {
 
     template<typename T>
     auto operator co_await(tasklet<T> const &task) {
-        struct awaitable : public has_slots {
-            std::experimental::coroutine_handle<> awaiting = nullptr;
-            tasklet<T> const &task;
-
-            explicit awaitable(tasklet<T> const &t) : task(t) {
-                task.coro.promise().complete.connect(this, &awaitable::resolve);
-            }
-
-            awaitable(awaitable const &a) : task(a.task) {
-                task.coro.promise().complete.connect(this, &awaitable::resolve);
-            }
-
-            awaitable(awaitable &&other) noexcept : task(other.task) {
-                task.coro.promise().complete.connect(this, &awaitable::resolve);
-            }
-
-            bool await_ready() {
-                if (!task.started()) {
-                    // Need to start the task
-                    const_cast<tasklet<T> &>(task).start();
-                }
-                return !task.running();
-            }
-
-            void await_suspend(std::experimental::coroutine_handle<> h) {
-                // The awaiting coroutine is already suspended.
-                awaiting = h;
-            }
-
-            auto await_resume() {
-                return task.get();
-            }
-
-            void resolve() {
-                if (awaiting) awaiting.resume();
-            }
-        };
-        return awaitable(task);
+        return internal::awaitable<T>(task);
     }
 }
 
